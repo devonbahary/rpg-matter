e2c9eaf7f4ef515877bd57b9a436031538a312b1
devonbahary <devonbahary@gmail.com>
2020-05-07 00:17:25 -0700

    adds Game_Party.onPlayerWalk() call when the Game_Party._steps count reaches the next integer in increaseSteps()

--- src/rpg-objects/Game_Party.js
+++ src/rpg-objects/Game_Party.js
@@ -9,5 +9,12 @@ Game_Party.prototype.steps = function() {
 };
 
 Game_Party.prototype.increaseSteps = function(steps) {
-    this._steps += steps;
+    const prevSteps = this._steps;
+    this._steps += steps; // overwrite, used to just ++
+
+    if (!$gameMap.isEventRunning()) { // onPlayerWalk() used to only be called from Game_Player during this condition
+        for (let i = 0; i < Math.floor(this._steps) - Math.floor(prevSteps); i++) {
+            this.onPlayerWalk();
+        }
+    }  
 };
