dc7248d6f38934b4c41e8b238816403e4c3a7371
devonbahary <devonbahary@gmail.com>
2020-05-10 11:31:19 -0700

    recognize playtest mode from ENV

--- src/matter-core/rpg-objects/Game_Temp.js
+++ src/matter-core/rpg-objects/Game_Temp.js
@@ -1,7 +1,14 @@
+import { get } from "lodash";
+
 //-----------------------------------------------------------------------------
 // Game_Temp
 //
 // The game object class for temporary data that is not included in save data.
+
+const _Game_Temp_isPlaytest = Game_Temp.prototype.isPlaytest;
+Game_Temp.prototype.isPlaytest = function() {
+    return _Game_Temp_isPlaytest.call(this) || ENV === 'development';
+};
 
 const _Game_Temp_setDestination = Game_Temp.prototype.setDestination;
 Game_Temp.prototype.setDestination = function(x, y) {
