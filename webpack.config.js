4a35c42b6d5e3d6fceec09394f6031db913e7f7c
devonbahary <devonbahary@gmail.com>
2020-05-10 11:31:05 -0700

    updates package.json scripts and webpack.config.js to include ENV

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
+const webpack = require('webpack');
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    MatterCore: './src/matter-core/MatterCore.js',
-    MatterActionEvent: './src/matter-action-event/MatterActionEvent.js',
-  },
-  output: {
-    path: path.join(__dirname, 'rpgmakermv', 'js', 'plugins'),
-    filename: '[name].js',
-  },
-  devtool: 'inline-source-map',
-  target: 'web',
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/,
-    }]
-  }
+module.exports = env => {
+  return {
+    mode: 'development',
+    entry: {
+      MatterCore: './src/matter-core/MatterCore.js',
+      MatterActionEvent: './src/matter-action-event/MatterActionEvent.js',
+    },
+    output: {
+      path: path.join(__dirname, 'rpgmakermv', 'js', 'plugins'),
+      filename: '[name].js',
+    },
+    devtool: 'inline-source-map',
+    target: 'web',
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "ENV": JSON.stringify(env.ENV),
+      }),
+    ],
+  };
 };
