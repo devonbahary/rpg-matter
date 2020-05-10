47612332cbf5aacb7fd5229823b504312826b3ce
devonbahary <devonbahary@gmail.com>
2020-05-09 18:24:18 -0700

    introduces vectorLengthFromAToB() to /utils/vector.js

--- src/matter-core/utils/vector.js
+++ src/matter-core/utils/vector.js
@@ -1,3 +1,4 @@
 import { Vector } from "matter-js";
 
 export const vectorFromAToB = (a, b) => Vector.add(b, Vector.neg(a));
+export const vectorLengthFromAToB = (a, b) => Vector.magnitude(vectorFromAToB(a, b));
