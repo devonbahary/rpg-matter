1816bf8696ac271f636f26ffefcf67ee6d8daa8b
devonbahary <devonbahary@gmail.com>
2020-05-14 02:01:59 -0700

    formatting

--- src/matter-core/matter/collision-on-rotate-bugfix.js
+++ src/matter-core/matter/collision-on-rotate-bugfix.js
@@ -2,19 +2,19 @@ import { Pair } from "matter-js";
 
 const _Pair_update = Pair.update;
 Pair.update = (pair, collision, timestamp) => {
-  _Pair_update(pair, collision, timestamp);
-  const { parentA, parentB } = collision;
-  if (parentA.isTurning || parentB.isTurning) Pair.setActive(pair, false, timestamp);
+    _Pair_update(pair, collision, timestamp);
+    const { parentA, parentB } = collision;
+    if (parentA.isTurning || parentB.isTurning) Pair.setActive(pair, false, timestamp);
 };
 
 const _Game_CharacterBase_setDirection = Game_CharacterBase.prototype.setDirection;
 Game_CharacterBase.prototype.setDirection = function(d) {
-  _Game_CharacterBase_setDirection.call(this, d);
-  this.body.isTurning = true;
+    _Game_CharacterBase_setDirection.call(this, d);
+    this.body.isTurning = true;
 };
 
 const _Game_CharacterBase_update = Game_CharacterBase.prototype.update;
 Game_CharacterBase.prototype.update = function() {
-    this.body.isTurning = false;
+    if (this.body.isTurning) this.body.isTurning = false;
     _Game_CharacterBase_update.call(this);
 };
