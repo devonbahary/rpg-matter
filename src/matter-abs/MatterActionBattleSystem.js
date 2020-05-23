1f83782e0fa5e8755cb282fde70caf6f27f7f812
devonbahary <devonbahary@gmail.com>
2020-05-23 16:58:25 -0700

    introduces Game_CharacterBase.battler property

--- src/matter-abs/MatterActionBattleSystem.js
+++ src/matter-abs/MatterActionBattleSystem.js
@@ -7,3 +7,14 @@
  * Requires MatterCore.js plugin.
 */
 
+//-----------------------------------------------------------------------------
+// Game_CharacterBase
+//
+// The superclass of Game_Character. It handles basic information, such as
+// coordinates and images, shared by all characters.
+
+const _Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers;
+Game_CharacterBase.prototype.initMembers = function() {
+    _Game_CharacterBase_initMembers.call(this);
+    this.battler = null;
+};
