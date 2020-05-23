48ae16d28cb7d9976d98b4c0d7a8cecc571eaaac
devonbahary <devonbahary@gmail.com>
2020-05-23 16:58:51 -0700

    sets up Game_Event.battler from page settings

--- src/matter-abs/MatterActionBattleSystem.js
+++ src/matter-abs/MatterActionBattleSystem.js
@@ -7,6 +7,12 @@
  * Requires MatterCore.js plugin.
 */
 
+import { battlerFromPage } from "./utils";
+
+export const EVENT_TAG_REGEX_ACTOR_ID = /\<Actor (\d+)\>/i;
+export const EVENT_TAG_REGEX_ENEMY_ID = /\<Enemy (\d+)\>/i;
+
+
 //-----------------------------------------------------------------------------
 // Game_CharacterBase
 //
@@ -17,4 +23,43 @@ const _Game_CharacterBase_initMembers = Game_CharacterBase.prototype.initMembers
 Game_CharacterBase.prototype.initMembers = function() {
     _Game_CharacterBase_initMembers.call(this);
     this.battler = null;
+};
+
+//-----------------------------------------------------------------------------
+// Game_Event
+//
+// The game object class for an event. It contains functionality for event page
+// switching and running parallel process events.
+
+const _Game_Event_clearPageSettings = Game_Event.prototype.clearPageSettings;
+Game_Event.prototype.clearPageSettings = function() {
+    _Game_Event_clearPageSettings.call(this);
+    this.battler = null;
+};
+
+const _Game_Event_setupPageSettings = Game_Event.prototype.setupPageSettings;
+Game_Event.prototype.setupPageSettings = function() {
+    _Game_Event_setupPageSettings.call(this);
+    this.setupBattler();
+    /*
+        - setImage
+        - setDirection
+        - setPattern
+        - setMoveSpeed (would be handled by battler anyway?)
+        - setMoveFrequency
+        - setPriorityType
+        - setWalkAnime 
+        - setStepAnime (stepping enemies are engaged with the user?)
+        - setDirectionFix
+        - setThrough
+        - setMoveRoute (define move pattern based on Game_Enemy?)
+        - _moveType ^
+        - _trigger (should be none, except maybe for NPC actors?)
+        - 
+    */
+};
+
+Game_Event.prototype.setupBattler = function() {
+    const battler = battlerFromPage.call(this);
+    if (battler) this.battler = battler;
 };
---
+++ src/matter-abs/utils.js
@@ -0,0 +1,48 @@
+import { EVENT_TAG_REGEX_ACTOR_ID, EVENT_TAG_REGEX_ENEMY_ID } from "./MatterActionBattleSystem";
+
+function battlerSetupError(msg) {
+    throw new Error(msg + ` for event ID ${this.eventId()}`);
+};
+
+function battlerIdFromPage(regex) {
+    return this.pageComments().reduce((battlerId, comment) => {
+        const match = comment.match(regex);
+        if (match && battlerId) {
+            battlerSetupError.call(this, `found multiple battler IDs in single page`);
+        } else if (match) {
+            return match[1];
+        }
+        return battlerId;
+    }, 0);
+};
+
+export function battlerFromPage(battlerType) {
+    let actor, enemy;
+    for (battlerType of [ 'actor', 'enemy' ]) {
+        let dataset, regex;
+        switch (battlerType) {
+            case 'actor':
+                dataset = $dataActors;
+                regex = EVENT_TAG_REGEX_ACTOR_ID;
+                break;
+            case 'enemy':
+                dataset = $dataEnemies;
+                regex = EVENT_TAG_REGEX_ENEMY_ID;
+                break;
+            default:
+                throw new Error(`invalid argument ${battlerType} for battlerType; must be 'actor' or 'enemy'`);
+        }
+    
+        const battlerId = battlerIdFromPage.call(this, regex);
+        if (!battlerId) continue;
+    
+        const battler = dataset[battlerId];
+        if (!battler) battlerSetupError.call(this, `cannot create ${battlerType} with ${battlerType} ID ${battlerId}`);
+    
+        if (battlerType === 'actor') actor = new Game_Actor(battlerId);
+        else if (battlerType === 'enemy') enemy = new Game_Enemy(battlerId);
+    }
+    
+    if (actor && enemy) battlerSetupError.call(this, `found both actor and enemy IDs in single page`);
+    return actor || enemy;
+}
