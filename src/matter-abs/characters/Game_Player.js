9bfe7ad94a1de19c6404651c8daa92368e74181b
devonbahary <devonbahary@gmail.com>
2020-05-27 19:40:57 -0700

    set party leader as Game_Player.battler on Game_Player.refresh()

--- src/matter-abs/characters/Game_Player.js
+++ src/matter-abs/characters/Game_Player.js
@@ -4,9 +4,11 @@
 // The game object class for the player. It contains event starting
 // determinants and map scrolling functions.
 
-Object.defineProperties(Game_Player.prototype, {
-    battler: { get: function() { return $gameParty.leader(); }, configurable: false },
-});
+const _Game_Player_refresh = Game_Player.prototype.refresh;
+Game_Player.prototype.refresh = function() {
+    _Game_Player_refresh.call(this);
+    this.setBattler($gameParty.leader());
+};
 
 const _Game_Player_canMove = Game_Player.prototype.canMove;
 Game_Player.prototype.canMove = function() {
