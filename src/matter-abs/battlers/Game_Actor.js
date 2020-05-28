27f1804f1b4d79e0f46c17a21b21ebe9cdb01842
devonbahary <devonbahary@gmail.com>
2020-05-27 20:38:02 -0700

    adds Game_Actor.weapon getter

--- src/matter-abs/battlers/Game_Actor.js
+++ src/matter-abs/battlers/Game_Actor.js
@@ -4,7 +4,14 @@
 // The game object class for an actor.
 
 Object.defineProperties(Game_Actor.prototype, {
-    weaponIconIndex: { get: function() { return this.weapons().length ? this.weapons()[0].iconIndex : 0; }, configurable: false },
+    weapon: { get: function() { 
+        return this._equips.reduce((weapon, item) => {
+            if (weapon) return weapon;
+            if (DataManager.isWeapon(item.object())) return item;
+            return weapon;
+        }, null);
+    }, configurable: false },
+    weaponIconIndex: { get: function() { return this.weapon ? this.weapon.object().iconIndex : 0; }, configurable: false },
 });
 
 Game_Actor.prototype.isFriendWith = function(battler) {
