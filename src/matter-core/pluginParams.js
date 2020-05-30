c545b5fbefeb2f2dfd9d9d669998d0333567bf4f
devonbahary <devonbahary@gmail.com>
2020-05-30 15:36:08 -0700

    reordering

--- src/matter-core/pluginParams.js
+++ src/matter-core/pluginParams.js
@@ -1,16 +1,16 @@
 const MATTER_CORE = {};
 
 MATTER_CORE.BASE_MOVE_SPEED = 0.25 / (8 - parseInt(PluginManager.parameters('MatterCore')["Base Move Speed"]));
-MATTER_CORE.IS_CLICK_TO_MOVE_DASH = JSON.parse(PluginManager.parameters('MatterCore')["Click To Move Dash"]);
 MATTER_CORE.DASH_SPEED_BOOST = Number(PluginManager.parameters('MatterCore')["Dash Speed Boost"]);
 MATTER_CORE.EVENT_TOUCH_COOLDOWN = Number(PluginManager.parameters('MatterCore')["Touch Event Cooldown"]);
 MATTER_CORE.INTERACTION_RADIUS = Number(PluginManager.parameters('MatterCore')["Interaction Radius"]);
+MATTER_CORE.IS_CLICK_TO_MOVE_DASH = JSON.parse(PluginManager.parameters('MatterCore')["Click To Move Dash"]);
 MATTER_CORE.PLAYTEST_IS_TIMESCALE_ADJUST = JSON.parse(PluginManager.parameters('MatterCore')["Playtest Timescale On Button Press"]);
-MATTER_CORE.PLAYTEST_TIMESCALE_KEY_CODE = Number(PluginManager.parameters('MatterCore')["Playtest Timescale Key Code"]);
 MATTER_CORE.PLAYTEST_TIMESCALE = Number(PluginManager.parameters('MatterCore')["Playtest Timescale"]);
+MATTER_CORE.PLAYTEST_TIMESCALE_KEY_CODE = Number(PluginManager.parameters('MatterCore')["Playtest Timescale Key Code"]);
 MATTER_CORE.RENDER_IS_DISPLAY = JSON.parse(PluginManager.parameters('MatterCore')["Display Render"]);
-MATTER_CORE.RENDER_WIDTH = parseInt(PluginManager.parameters('MatterCore')["Render Width"]);
 MATTER_CORE.RENDER_HEIGHT = parseInt(PluginManager.parameters('MatterCore')["Render Height"]);
+MATTER_CORE.RENDER_WIDTH = parseInt(PluginManager.parameters('MatterCore')["Render Width"]);
 MATTER_CORE.TILE_SIZE = parseInt(PluginManager.parameters('MatterCore')["Tile Size"]);
 
 export default MATTER_CORE;
