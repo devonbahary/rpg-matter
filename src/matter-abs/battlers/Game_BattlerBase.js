8db63983da113b66044de872e72b6c3a69e6415d
devonbahary <devonbahary@gmail.com>
2020-05-30 20:11:10 -0700

    adds Game_BattlerBase uuid as .id

--- src/matter-abs/battlers/Game_BattlerBase.js
+++ src/matter-abs/battlers/Game_BattlerBase.js
@@ -3,5 +3,13 @@
 //
 // The superclass of Game_Battler. It mainly contains parameters calculation.
 
+import { v4 as uuidv4 } from "uuid";
+
+const _Game_BattlerBase_initMembers = Game_BattlerBase.prototype.initMembers;
+Game_BattlerBase.prototype.initMembers = function() {
+    _Game_BattlerBase_initMembers.call(this);
+    this.id = uuidv4();
+};
+
 Game_BattlerBase.prototype.update = function() {
 };
