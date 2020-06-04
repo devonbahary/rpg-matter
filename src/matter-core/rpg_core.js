7538e68abf0dd81596adadf165fa68dc8274b995
devonbahary <devonbahary@gmail.com>
2020-06-04 01:45:09 -0700

    adds additional keys to Input.keyMapper

--- src/matter-core/rpg_core.js
+++ src/matter-core/rpg_core.js
@@ -16,6 +16,20 @@ Number.prototype.isInRange = function(min, max) {
  * @class Input
  */
 
+Input.keyMapper = {
+    ...Input.keyMapper,
+    65: 'A',        // A
+    67: 'C',        // C
+    68: 'D',        // D
+    69: 'E',        // E
+    81: 'Q',        // Q
+    82: 'R',        // R
+    83: 'S',        // S
+    86: 'V',        // V
+    87: 'W',        // W
+    88: 'X',        // X
+    90: 'Z',        // Z
+};
 
 Input.isPlaytestTimescalePressed = function() {
     if (!$gameTemp.isPlaytest() || !MATTER_CORE.PLAYTEST_IS_TIMESCALE_ADJUST) return false;
