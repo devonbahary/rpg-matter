ede33c017eddf6f0485eef55d7d856ba5b0e7907
devonbahary <devonbahary@gmail.com>
2020-06-20 13:43:17 -0700

    plays weapon sprite animations with respect to their orientation (direction)

--- src/matter-abs/sprites/Sprite_Animation.js
+++ src/matter-abs/sprites/Sprite_Animation.js
@@ -21,3 +21,53 @@ Sprite_Animation.prototype.shouldUpdate = function() {
 Sprite_Animation.prototype.isAnimationPaused = function() {
     return this._target.isAnimationPaused();
 };
+
+const _Sprite_Animation_updateCellSprite = Sprite_Animation.prototype.updateCellSprite;
+Sprite_Animation.prototype.updateCellSprite = function(sprite, cell) {
+    _Sprite_Animation_updateCellSprite.call(this, sprite, cell);
+    this.updateCellWeaponSpriteAnimation(sprite, cell);
+};
+
+Sprite_Animation.prototype.updateCellWeaponSpriteAnimation = function(sprite, cell) {
+    const pattern = cell[0];
+
+    if (pattern >= 0 && this._target instanceof Sprite_CharacterWeapon) {
+        let mirror = this._mirror;
+
+        const aliasedMirrorAndRotation = () => {
+            if (cell[5]) {
+                sprite.scale.x *= -1;
+            }
+            if (mirror) {
+                sprite.x *= -1;
+                sprite.rotation *= -1;
+                sprite.scale.x *= -1;
+            }
+        };
+        
+        aliasedMirrorAndRotation();
+    
+        switch (this._target._character.direction()) {
+            case 2:
+                break;
+            case 4:
+                sprite.rotation += 90 * Math.PI / 180;
+                sprite.x = -cell[2];
+                sprite.y = cell[1];
+                break;
+            case 6:
+                mirror = !mirror;
+                sprite.rotation += 90 * Math.PI / 180;
+                sprite.x = -cell[2];
+                sprite.y = cell[1];
+                break;
+            case 8:
+                sprite.rotation += 180 * Math.PI / 180;
+                sprite.y = -cell[2];
+                sprite.x = -cell[1];
+                break;
+        }
+    
+        aliasedMirrorAndRotation();
+    }
+};
