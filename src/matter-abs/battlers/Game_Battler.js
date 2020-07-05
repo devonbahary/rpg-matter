2937307d6911420e29d5f1d94a789690b3e6ace7
devonbahary <devonbahary@gmail.com>
2020-07-05 12:34:07 -0700

    directory restructure

--- src/matter-abs/battlers/Game_Battler.js
+++ src/matter-abs/battlers/Game_Battler.js
@@ -4,7 +4,7 @@
 // The superclass of Game_Actor and Game_Enemy. It contains methods for sprites
 // and actions.
 
-import { Root as BehaviorTree } from "./behavior-tree/sequences";
+import { Root as BehaviorTree } from "./behavior-tree";
 import { getMassFromMeta, getBooleanFromMeta } from "../../utils";
 import MATTER_CORE from "../../matter-core/pluginParams";
 import MATTER_ABS from "../MatterActionBattleSystem";
