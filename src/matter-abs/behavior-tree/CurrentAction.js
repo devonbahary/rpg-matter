27cbc953146675094fd1b9d4ecf9eb6434145c63
devonbahary <devonbahary@gmail.com>
2020-07-06 01:37:27 -0700

    prevents Guard from being channeled longer than 180 frames at a time

--- src/matter-abs/behavior-tree/CurrentAction.js
+++ src/matter-abs/behavior-tree/CurrentAction.js
@@ -1,11 +1,24 @@
-import { Sequence } from "./types";
+import { Sequence, Leaf } from "./types";
 import { HasAction, TurnTowardTarget } from "./shared";
+import STATUSES from "./statuses";
 
 export class CurrentAction extends Sequence {
     constructor(behaviorTree) {
         super(behaviorTree, [
             HasAction,
             TurnTowardTarget,
+            MaintainAction,
         ]);     
     }
 };
+
+class MaintainAction extends Leaf {
+    constructor(behaviorTree) {
+        super(behaviorTree);
+    }
+    
+    tick() {
+        if (this.action.isGuard() && this.behaviorTree.actionDuration > 180) this.battler.clearAction();
+        return STATUSES.RUNNING;
+    }
+}
