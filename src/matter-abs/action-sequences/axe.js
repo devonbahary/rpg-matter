a7ae15170b7e4f691e5628ca360fdcd3a4500c63
devonbahary <devonbahary@gmail.com>
2020-07-13 00:40:57 -0700

    adds WILD_SWING aciton sequence

--- src/matter-abs/action-sequences/axe.js
+++ src/matter-abs/action-sequences/axe.js
@@ -3,6 +3,10 @@ import { WEAPON_POSES } from "../weapon-poses";
 
 const { 
     APPLY_EFFECT,
+    ROUTE_DIR_FIX_ON: DIR_FIX_ON,
+    ROUTE_DIR_FIX_OFF: DIR_FIX_OFF,
+    ROUTE_TURN_90D_L: TURN_90D_L,
+    ROUTE_TURN_90D_R: TURN_90D_R,
     STEP_BACKWARD,
     STEP_FORWARD, 
     STEP_NEUTRAL, 
@@ -14,5 +18,12 @@ export default {
         16: [ STEP_NEUTRAL, weaponPose(WEAPON_POSES.EXTEND), animationSelf(11, true), playSe('Wind7', 90, 60) ],
         18: [ STEP_FORWARD, weaponPose(WEAPON_POSES.DOWN_SWING_AXE), APPLY_EFFECT ],
         35: [],
+    },
+    WILD_SWING: {
+        1: [ stepLock(true), STEP_BACKWARD, TURN_90D_L, weaponPose(WEAPON_POSES.EXTEND), animationSelf(33, true), playSe('Wind7', 90, 50), DIR_FIX_ON ],
+        6: [ DIR_FIX_OFF, TURN_90D_R, weaponPose(WEAPON_POSES.EXTEND), DIR_FIX_ON ],
+        7: APPLY_EFFECT,
+        8: [ weaponPose(WEAPON_POSES.SIDE) ],
+        60: [],
     },
 };
